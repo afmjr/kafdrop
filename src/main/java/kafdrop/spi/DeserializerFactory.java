428bf9ac19a40514745b850cd45a7cccec861cce
Björn Tennander <bjorn.tennander@yahoo.com>
2021-01-14 13:22:44 +0100

    Vermiculus: Adjust for easy deploy

--- src/main/java/kafdrop/spi/DeserializerFactory.java
+++
@@ -1,9 +0,0 @@
-package kafdrop.spi;
-
-import org.apache.kafka.common.serialization.Deserializer;
-
-public interface DeserializerFactory {
-    Deserializer<Object> getDeserializer(String topicName);
-
-    boolean supportsTopic(String topicName);
-}
